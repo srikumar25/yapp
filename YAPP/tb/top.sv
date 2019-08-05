1f93b19199d6caffffe4216aed7dce6828dcc049
srikumar25 <53485118+srikumar25@users.noreply.github.com>
2019-08-05 17:49:22 +0530

    Having uvm_env addition in TB top file

--- YAPP/tb/top.sv
+++ YAPP/tb/top.sv
@@ -1,46 +1,59 @@
 
 `timescale 1ns/1ps
 
-`include "../common/common_pkg.sv"
-
 module top();
 
-
 import uvm_pkg::*;
-import chan_pkg::*;
-import hbus_pkg::*;
-import yapp_pkg::*;
+//import chan_pkg::*;
+//import hbus_pkg::*;
 import common_pkg::*;
+import yapp_pkg::*;
 
-intf i1;
+yapp_seq_item seq_item;
 
+
+common i1();
+yapp_env e1;
 
 yapp_dut y1(
 		// Common Signals
-		clk,
-		reset,
+		clk(i1.clk),
+		reset(i1.reset),
 		// Yapp interface
-                in_data,
-		in_data_vld,
-		in_suspend,
+                in_data(),
+		in_data_vld(),
+		in_suspend(),
 		// Channels
-                data_0,data_vld_0,suspend_0,
-                data_1,data_vld_1,suspend_1,
-                data_2,data_vld_2,suspend_2,
+                data_0(),data_vld_0(),suspend_0(),
+                data_1(),data_vld_1(),suspend_1(),
+                data_2(),data_vld_2(),suspend_2(),
 		// Hbus interface
-                hdata_in,
-		hdata_out,
+                hdata_in(),
+		hdata_out(),
 		haddr,hen,
 		hw_rd,
-                Error
+                Error(i1.error)
 		);
+
+initial
+begin
+	e1 = yapp_env::type_id::create("e1");
+	e1.y1_item.randomize();
+	e1.y1_item.print();
+end
 
 initial
 begin
 	i1.clk = 1'b0;
 	i1.reset = 1'b0;
 	#50 i1.reset = 1'b1;
+	e1.y1_item.print();
 	#100 $finish;
+end
+
+initial
+begin
+	run_test();
 end
 
 always #10 i1.clk = ~i1.clk;
