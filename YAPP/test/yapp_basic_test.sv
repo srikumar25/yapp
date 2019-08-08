9972ecb2e64f670609d8221a8909b21dfa1d9c57
srikumar25 <53485118+srikumar25@users.noreply.github.com>
2019-08-08 13:53:10 +0530

    Add files via upload

--- YAPP/test/yapp_basic_test.sv
+++ YAPP/test/yapp_basic_test.sv
@@ -1,1 +1,25 @@
 
+class yapp_basic_test extends uvm_test;
+	`uvm_component_utils(yapp_basic_test)
+
+yapp_env env;
+
+
+function new(string name="yapp_basic_test",uvm_component parent);
+	super.new(name,parent);
+endfunction
+
+virtual function void build_phase(uvm_phase phase);
+	super.build_phase(phase);
+	env = yapp_env::type_id::create("env",this);
+endfunction
+
+task run_phase(uvm_phase phase);
+	phase.raise_objection(this);
+		#10;
+		env.y1_item.randomize();
+		env.y1_item.print();
+	phase.drop_objection(this);
+endtask
+
+endclass
