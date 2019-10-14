ed1e5f259c5a12f458f6ee2f9c6f4771cdcebd85
srikumar25 <53485118+srikumar25@users.noreply.github.com>
2019-10-14 13:32:44 +0530

    Add files via upload

--- YAPP/test/yapp_basic_test.sv
+++ YAPP/test/yapp_basic_test.sv
@@ -5,6 +5,7 @@ class yapp_basic_test extends uvm_test;
 yapp_env env;
 yapp_basic_seq seq;
 hbus_basic_seq h_seq;
+chan_basic_seq c_seq;
 virtual yapp_if y1_vif;
 
 
@@ -19,36 +20,70 @@ virtual function void build_phase(uvm_phase phase);
 		`uvm_info("BASIC_TEST",$sformatf("Not obtained the interface"),UVM_LOW)
 	else
 		`uvm_info("BASIC_TEST",$sformatf("Obtained the interface in test too"), UVM_LOW)
+    	uvm_config_wrapper::set(this, "env.c0_agt.c_sqr.run_phase",
+                            "default_sequence",
+                            chan_basic_seq::type_id::get());
+    	uvm_config_wrapper::set(this, "env.c1_agt.c_sqr.run_phase",
+                            "default_sequence",
+                            chan_basic_seq::type_id::get());
+    	uvm_config_wrapper::set(this, "env.c2_agt.c_sqr.run_phase",
+                            "default_sequence",
+                            chan_basic_seq::type_id::get());
+endfunction
+
+function void end_of_elaboration();
+	uvm_top.print_topology();
 endfunction
 
 
-task configure_phase(uvm_phase phase);
+/*task configure_phase(uvm_phase phase);
 	phase.raise_objection(this);
 	`uvm_info("BASIC_TEST_CONFIGURE_PHASE",$sformatf("Trying to configure the HBUS interface"),UVM_LOW)
 	h_seq = hbus_basic_seq::type_id::create("h_seq");
 	`uvm_info("BASIC_TEST_CONFIGURE_PHASE",$sformatf("Value of HBUS_SEQ instance ID is %d",h_seq.get_inst_id()),UVM_LOW)
 	h_seq.start(env.h1_agt.h_sqr);
-	h_seq.req.print();
+	//h_seq.req.print();
 	phase.drop_objection(this); 
-endtask 
+endtask  */
 
-
-//task run_phase(uvm_phase phase);
-task main_phase(uvm_phase phase);
+task run_phase(uvm_phase phase);
+//task main_phase(uvm_phase phase);
 	phase.raise_objection(this);
-	seq = yapp_basic_seq::type_id::create("seq");
-	//seq.y1_sqr = env.y1_agt.y_sqr;
+
 	`uvm_info("BASIC_TEST",$sformatf("Value of env object instance ID is %d",env.get_inst_id()),UVM_LOW)
-	seq.start(env.y1_agt.y_sqr);
-		//#10;
-		`uvm_info("BASIC_TEST",$sformatf("Inside the basic test's run phase"),UVM_LOW)
-		//`uvm_info("BASIC_TEST", $sformatf("Let's hope this is the one"),UVM_LOW)
-		//void'(env.y1_item.randomize());
-		//#10;
-		seq.req.print();
-		//env.y1_item.print();
+	//`uvm_info("BASIC_TEST_CONFIGURE_PHASE",$sformatf("Trying to configure the HBUS interface"),UVM_LOW)
+	seq = yapp_basic_seq::type_id::create("seq");
+	h_seq = hbus_basic_seq::type_id::create("h_seq");
+	c_seq = chan_basic_seq::type_id::create("c_seq");
+	`uvm_info("BASIC_TEST_CONFIGURE_PHASE",$sformatf("Value of HBUS_SEQ instance ID is %d",h_seq.get_inst_id()),UVM_LOW)
+	h_seq.start(env.h1_agt.h_sqr);
+/*	fork
+		begin
+			`uvm_info("C0_SEQ",$sformatf("Let's see if c_seq gets started"),UVM_LOW)
+			c_seq.start(env.c0_agt.c_sqr);
+		end
+		begin
+			`uvm_info("C1_SEQ",$sformatf("Let's see if c_seq gets started"),UVM_LOW)
+			c_seq.start(env.c1_agt.c_sqr);
+		end
+		begin
+			`uvm_info("C2_SEQ",$sformatf("Let's see if c_seq gets started"),UVM_LOW)
+			c_seq.start(env.c2_agt.c_sqr);
+		end */
+//		begin
+			seq.start(env.y1_agt.y_sqr);
+				//#10;
+				`uvm_info("BASIC_TEST",$sformatf("Inside the basic test's run phase"),UVM_NONE)
+				//`uvm_info("BASIC_TEST", $sformatf("Let's hope this is the one"),UVM_LOW)
+				//void'(env.y1_item.randomize());
+				//#10;
+			seq.req.print();
+				//env.y1_item.print();
+//		end
+//		join_any
+		uvm_test_done.set_drain_time(this,5000ns);
 	phase.drop_objection(this);
-	phase.phase_done.set_drain_time(this,5000ns);
+	//phase.phase_done.set_drain_time(this,5000ns);
 endtask
 
 endclass
