6a33825680c0df7daa5851b78292a2b6346bfede
srikumar25 <53485118+srikumar25@users.noreply.github.com>
2019-10-14 13:32:09 +0530

    Add files via upload

--- YAPP/env/yapp_env.sv
+++ YAPP/env/yapp_env.sv
@@ -1,1 +1,28 @@
 
+class yapp_env extends uvm_env;
+	
+	`uvm_component_utils(yapp_env)
+
+
+yapp_seq_item y1_item;
+yapp_agent y1_agt,y0_agt;
+
+function new(string name="yapp_env",uvm_component parent);
+//function new(string name,uvm_component parent);
+	super.new(name,parent);
+endfunction
+
+function void build_phase(uvm_phase phase);
+	super.build_phase(phase);
+	y1_item = yapp_seq_item::type_id::create("y1_item");
+	y1_agt = yapp_agent::type_id::create("y1_agt",this);
+endfunction
+
+function void connect_phase(uvm_phase phase);
+	super.connect_phase(phase);
+endfunction
+
+task run_phase(uvm_phase phase);
+endtask
+
+endclass
